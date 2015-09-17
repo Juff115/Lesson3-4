c2160e3296a594f41730ce3c29e6ac5ab3b33d58
jame5380 <jame5380@ISS147D8-24.scdsb.pri>
2015-09-17 10:36:49 -0400

    proggres

--- src/SalaryTable.java
+++ src/SalaryTable.java
@@ -1,20 +1,30 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+import hsa.*;
+import BreezySwing.Format;
 
-/**
- *
- * @author jame5380
- */
 public class SalaryTable {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        // TODO code application logic here
+        Console c= new Console();
+        Format f = new Format();
+        int oldsal=0, newsal=40000, raise=40000;
+        c.print(Format.justify('r', "YEAR", 5));
+        c.print(Format.justify('r', "OLD SALARY", 15));
+        c.print(Format.justify('r', "RAISE", 15));
+        c.println(Format.justify('r', "NEW SALARY", 20));
+        c.print(Format.justify('r', "----", 5));
+        c.print(Format.justify('r', "----------", 15));
+        c.print(Format.justify('r', "-----", 15));
+        c.println(Format.justify('r', "----------", 20));
+        for (int x=0; x<10; x++){
+            c.print(Format.justify('c', x+1, 5));
+            c.print(Format.justify('c', "$"+oldsal, 20));
+            c.print(Format.justify('c', "$"+ raise, 15));
+            c.println(Format.justify('r', "$"+newsal, 15));
+            oldsal=newsal;
+            newsal*=1.03;
+            raise= newsal-oldsal;
+                
+        }
     }
     
 }
