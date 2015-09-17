d7a8b67946814952211151afecb0fc14af1cc5e6
jame5380 <jame5380@ISS147D8-24.scdsb.pri>
2015-09-17 09:54:03 -0400

    Divide finished

--- src/Divide.java
+++ src/Divide.java
@@ -1,20 +1,26 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
-/**
- *
- * @author jame5380
- */
+import hsa.*;
 public class Divide {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        // TODO code application logic here
+        Console c= new Console();
+        int div=0, remain=0;
+        int[] num;
+        num = new int[2];
+        for (int x=0; x<2; x++){
+            c.print("Enter num " + (x+1) +":");
+            num[x] = c.readInt();
+        }
+        if (num[0] <= num[1]){
+            div=num[1]/num[0];
+            remain=num[1]%num[0];
+            c.println(""+num[1]+" divided by "+num[0]+" is equal to "+div+" with a remainder of "+ remain);
+        }
+        else{
+            div=num[0]/num[1];
+            remain=num[0]%num[1];
+            c.println(""+num[0]+" divided by "+num[1]+" is equal to "+div+" with a remainder of "+ remain);
+        }
+        
     }
     
 }
