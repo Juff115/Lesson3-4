0798648b05949e4ec0e92108754993d76664d6cb
jame5380 <jame5380@ISS147D8-24.scdsb.pri>
2015-09-18 10:10:41 -0400

    PowersChart Done

--- src/PowersChart.java
+++ src/PowersChart.java
@@ -4,8 +4,19 @@ public class PowersChart {
 
     public static void main(String[] args) {
         Console c=new Console();
-        int expo;
-        
+        double expo, total;
+        while(true){
+            c.print("Enter the exponent for 2 between 1-10: ");
+            expo=c.readDouble();
+            if(expo>=0 & expo<=10){
+                break;
+            }
+            else{
+                c.print("Invalid\n");
+            }
+        }
+        total= (double) Math.pow(2,expo);
+        c.print("The answer is: "+total);
     }
     
 }
