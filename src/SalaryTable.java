3ef2ed0450581de7695cfa4ee94bef21b7b4f2b5
jame5380 <jame5380@ISS147D8-24.scdsb.pri>
2015-09-18 09:53:24 -0400

    SalaryTable Done

--- src/SalaryTable.java
+++ src/SalaryTable.java
@@ -8,18 +8,18 @@ public class SalaryTable {
         Format f = new Format();
         int oldsal=0, newsal=40000, raise=40000;
         c.print(Format.justify('r', "YEAR", 5));
-        c.print(Format.justify('r', "OLD SALARY", 15));
-        c.print(Format.justify('r', "RAISE", 15));
-        c.println(Format.justify('r', "NEW SALARY", 20));
+        c.print(Format.justify('r', "OLD SALARY", 14));
+        c.print(Format.justify('r', "RAISE", 11));
+        c.println(Format.justify('r', "NEW SALARY", 15));
         c.print(Format.justify('r', "----", 5));
-        c.print(Format.justify('r', "----------", 15));
-        c.print(Format.justify('r', "-----", 15));
-        c.println(Format.justify('r', "----------", 20));
+        c.print(Format.justify('r', "----------", 14));
+        c.print(Format.justify('r', "-----", 11));
+        c.println(Format.justify('r', "----------", 15));
         for (int x=0; x<10; x++){
-            c.print(Format.justify('c', x+1, 5));
-            c.print(Format.justify('c', "$"+oldsal, 20));
-            c.print(Format.justify('c', "$"+ raise, 15));
-            c.println(Format.justify('r', "$"+newsal, 15));
+            c.print(Format.justify('r', x+1, 4));
+            c.print(Format.justify('r', "$"+oldsal, 13));
+            c.print(Format.justify('r', "$"+ raise, 13));
+            c.println(Format.justify('r', "$"+newsal, 13));
             oldsal=newsal;
             newsal*=1.03;
             raise= newsal-oldsal;
